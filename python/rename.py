7316686a8076b6e259920a3035a21fa1ef3bcd37
Aso2001026 <2001026@s.asojuku.ac.jp>
2023-10-30 13:08:28 +0900

    Add comments

--- python/rename.py
+++ python/rename.py
@@ -2,6 +2,8 @@ import glob
 import os
 
 # Get all image files in images path
+# I puted not moving shuttle images in shuttle-not-moving folder
+# If you want to rename other images, change path
 path = '../image/shuttle-not-moving'
 
 file_count = 1
